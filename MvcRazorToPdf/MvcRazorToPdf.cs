c13c30fce333916ecf3841e6a08cb47958f7f2a6
Peter Dolkens <peter.dolkens@clubspark.com>
2023-09-05 20:41:44 +0100

    Add support for custom fonts Upgrade to SDK-style project file

--- MvcRazorToPdf/MvcRazorToPdf.cs
+++ MvcRazorToPdf/MvcRazorToPdf.cs
@@ -21,7 +21,43 @@ namespace MvcRazorToPdf
         /// <param name="viewName">ViewName</param>
         /// <param name="configureSettings">configuration settings</param>
         /// <returns></returns>
-        public byte[] GeneratePdfOutput(ControllerContext context, object model = null, string viewName = null,
+        public byte[] GeneratePdfOutput(ControllerContext context, object model = null, string viewName = null, Action<PdfWriter, Document> configureSettings = null)
+        {
+            if (viewName == null)
+            {
+                viewName = context.RouteData.GetRequiredString("action");
+            }
+
+            context.Controller.ViewData.Model = model;
+
+            byte[] output;
+            using (var document = new Document())
+            {
+                using (var workStream = new MemoryStream())
+                {
+                    PdfWriter writer = PdfWriter.GetInstance(document, workStream);
+                    writer.CloseStream = false;
+
+                    if (configureSettings != null)
+                    {
+                        configureSettings(writer, document);
+                    }
+                    document.Open();
+
+
+                    using (var reader = new MemoryStream(Encoding.UTF8.GetBytes(RenderRazorView(context, viewName))))
+                    {
+                        XMLWorkerHelper.GetInstance().ParseXHtml(writer, document, reader, null, FontFactory.FontImp as IFontProvider);
+
+                        document.Close();
+                        output = workStream.ToArray();
+                    }
+                }
+            }
+            return output;
+        }
+
+        public byte[] LegacyGeneratePdfOutput(ControllerContext context, object model = null, string viewName = null,
             Action<PdfWriter, Document> configureSettings = null)
         {
             if (viewName == null)
